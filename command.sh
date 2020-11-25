2376a12419c6856a5f08e49595e7c0fd98b15e30
dragonying <qfdy6688@gmail.com>
2020-11-25 13:37:18 +0800

    fuck

--- command.sh
+++ command.sh
@@ -1,3 +1,13 @@
-1. hello
-2. world
-3.you dafa
+#!/bin/bash
+#alias
+git config alias.lg "log --graph"
+git config alias.fuck  "add ." 
+
+
+#power
+#no ignore
+git config  core.fileMode true 
+#ignore
+git config  core.fileMode false
+#ignore whitespace
+git config --global apply.whitespace nowarn
